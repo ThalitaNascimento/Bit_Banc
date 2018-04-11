93bea776f0d0e543a28d1d5af7dfa66a4d8e83f4
Thalita Nascimento <thalitandeoliveira@gmail.com>
2018-04-11 11:39:45 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>BitBanc</title>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+   	<link rel="stylesheet" type="text/css" href="semantic/semantic.min.css">
+	<link rel="stylesheet" type="text/css" href="style/icons-site.css">
+   	
+
+   	<script
+   		  src="https://code.jquery.com/jquery-3.1.1.min.js"
+   		  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+   		  crossorigin="anonymous"></script>
+   	<script src="semantic/semantic.min.js"></script>
+
+    <!--Slick Slider-->
+    <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css">
+    
+
+</head>
+
+<body>
+<div class="pusher">
+  <div class="ui inverted vertical masthead center aligned segment">
+
+    <div class="ui container">
+    <div class="ui inverted large top fixed hidden menu">
+         <a href=""><img src="logo/logobranca.png" width="400px" height="90px"></a>
+        <div class="right item">
+
+          <a class="ui inverted button" href="login.php">ENTRAR</a>
+          
+          <div class="item"></div>
+
+          <a class="ui inverted button" href="cadastrar.php">CADASTRAR-SE </a>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="ui text container">
+      <h1 class="ui inverted header">
+        Gerenciador Financeiro
+      </h1>
+      <h2>BitBanc é uma ferramenta online para você controlar seus Gastos e Investimentos.</h2>
+      <div class="ui huge primary button">Site Acessivel</i></div>
+    </div>
+
+  </div>
+ 
+
+<!--  CARDS  -->
+<br> <br>
+<div class="ui center aligned header three column doubling stackable grid">
+  <div class="column"> </div>
+  
+  <div class="column">
+    <div class="ui yellow segment">COMO O BITBANC FUNCIONA?</div>
+  </div>
+  
+  <div class="column" id=" ">
+    
+     <img src="img/right-arrow.png"></div>
+  
+</div>
+
+</div>
+
+<div class="ui vertical stripe segment">
+    <div class="ui middle aligned stackable grid container">
+      <div class="row">
+        <div class="eight wide column">
+          <center><h1 class="ui header">Gerencie seus investimentos</h1></center>
+          <h4>Tenha total controle de todas as suas aplicações em fundos, renda fixa, renda variável e outros ativos.</h4>
+          
+        </div>
+        <div class="six wide right floated column">
+          <img src="assets/images/wireframe/white-image.png" class="ui medium bordered rounded image">
+        </div>
+      </div>
+      
+    </div>
+  </div>
+
+
+
+
+</body>
+</header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
